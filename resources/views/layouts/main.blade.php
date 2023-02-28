f17529d9616d2040d5dde12bc6b30167a4cc3ebf
Noah <spain.barcelona.1999@gmail.com>
2023-02-27 22:40:41 -0500

    Extends layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <title>@yield('title',' OneUI - Bootstrap 5 Admin Template &amp; UI Framework')</title>
+
+    <meta name="description"
+        content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="noindex, nofollow">
+
+    <!-- Open Graph Meta -->
+    <meta property="og:title" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework">
+    <meta property="og:site_name" content="OneUI">
+    <meta property="og:description"
+        content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+    <!-- END Icons -->
+
+    <!-- Stylesheets -->
+    <!-- OneUI framework -->
+    <link rel="stylesheet" id="css-main" href="{{ asset('css/oneui.min.css') }}">
+    @yield('css')
+
+    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+    <!-- <link rel="stylesheet" id="css-theme" href="{{ asset('css/themes/amethyst.min.css') }}"> -->
+    <!-- END Stylesheets -->
+</head>
+
+<body>
+    <div id="page-container"
+        class="sidebar-dark enable-page-overlay side-scroll page-header-fixed main-content-narrow">
+        <!-- Side Overlay-->
+       @include('partials.aside')
+        <!-- END Side Overlay -->
+
+        <!-- Header -->
+        @include('partials.header')
+        <!-- END Header -->
+
+        <!-- Main Container -->
+        <div >
+            @yield('content')
+        </div>
+        <!-- END Main Container -->
+
+        <!-- Footer -->
+        @include('partials.footer')
+        <!-- END Footer -->
+    </div>
+    <!-- END Page Container -->
+
+    <!--
+        OneUI JS
+
+        Core libraries and functionality
+        webpack is putting everything together at js/main/app.js
+    -->
+    <script src="{{ asset('js/oneui.app.min.js') }}"></script>
+    @yield('scripts')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,82 +1,24 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+@section('title','Página principal - Blog')
 
-    <title>OneUI - Bootstrap 5 Admin Template &amp; UI Framework</title>
+@section('css')
+@endsection
 
-    <meta name="description"
-        content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-    <meta name="author" content="pixelcave">
-    <meta name="robots" content="noindex, nofollow">
 
-    <!-- Open Graph Meta -->
-    <meta property="og:title" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework">
-    <meta property="og:site_name" content="OneUI">
-    <meta property="og:description"
-        content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+@section('content')
+    <!-- Hero Content -->
+    @include('partials.hero')
+    <!-- END Hero Content -->
 
-    <!-- Icons -->
-    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-    <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('media/favicons/favicon-192x192.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
-    <!-- END Icons -->
+    <!-- Page Content -->
+    @include('partials.content')
+    <!-- END Page Content -->
 
-    <!-- Stylesheets -->
-    <!-- OneUI framework -->
-    <link rel="stylesheet" id="css-main" href="{{ asset('css/oneui.min.css') }}">
+    <!-- Get Started -->
+    @include('partials.started')
+    <!-- END Get Started -->
+@endsection
 
-    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
-    <!-- <link rel="stylesheet" id="css-theme" href="{{ asset('css/themes/amethyst.min.css') }}"> -->
-    <!-- END Stylesheets -->
-</head>
-
-<body>
-    <div id="page-container"
-        class="sidebar-dark enable-page-overlay side-scroll page-header-fixed main-content-narrow">
-        <!-- Side Overlay-->
-       @include('partials.aside')
-        <!-- END Side Overlay -->
-
-        <!-- Header -->
-        @include('partials.header')
-        <!-- END Header -->
-
-        <!-- Main Container -->
-        <div >
-            <!-- Hero Content -->
-            @include('partials.hero')
-            <!-- END Hero Content -->
-
-            <!-- Page Content -->
-            @include('partials.content')
-            <!-- END Page Content -->
-
-            <!-- Get Started -->
-            @include('partials.started')
-            <!-- END Get Started -->
-        </div>
-        <!-- END Main Container -->
-
-        <!-- Footer -->
-        @include('partials.footer')
-        <!-- END Footer -->
-    </div>
-    <!-- END Page Container -->
-
-    <!--
-        OneUI JS
-
-        Core libraries and functionality
-        webpack is putting everything together at js/main/app.js
-    -->
-    <script src="{{ asset('js/oneui.app.min.js') }}"></script>
-</body>
-
-</html>
+@section('scripts')
+@endsection
