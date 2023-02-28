94424cbda0214193488d7c54a46fa0d99d312f52
Noah <spain.barcelona.1999@gmail.com>
2023-02-27 22:18:30 -0500

    Partials welcome

---
+++ resources/views/partials/content.blade.php
@@ -0,0 +1,240 @@
+<div class="content">
+    <div class="row">
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo8@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Top 10 Destinations
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Albert Ray</span> on July 16, 2021 · <span
+                            class="text-muted">10 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo12@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Follow Your Dreams
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Brian Stevens</span> on July 13, 2021 · <span
+                            class="text-muted">15 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo23@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Travel &amp; Work
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">David Fuller</span> on July 6, 2021 · <span
+                            class="text-muted">12 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo24@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Sleep Better
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Andrea Gardner</span> on July 21, 2021 · <span
+                            class="text-muted">9 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo4@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Black &amp; White
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Andrea Gardner</span> on July 16, 2021 · <span
+                            class="text-muted">5 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo6@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Exploring the clouds
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Brian Cruz</span> on July 1, 2021 · <span
+                            class="text-muted">3 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo7@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Explore the World
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Alice Moore</span> on May 19, 2021 · <span
+                            class="text-muted">9 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo8@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        Best choices of this year
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Carol Ray</span> on May 10, 2021 · <span
+                            class="text-muted">14 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+
+        <!-- Story -->
+        <div class="col-lg-4">
+            <a class="block block-rounded block-link-pop overflow-hidden"
+                href="be_pages_blog_story.html">
+                <img class="img-fluid" src="{{ asset('media/photos/photo9@2x.jpg') }}"
+                    alt="">
+                <div class="block-content">
+                    <h4 class="mb-1">
+                        The Secret Treasure
+                    </h4>
+                    <p class="fs-sm fw-medium mb-2">
+                        <span class="text-primary">Brian Stevens</span> on May 2, 2021 · <span
+                            class="text-muted">20 min</span>
+                    </p>
+                    <p class="fs-sm text-muted">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices, justo
+                        vel imperdiet gravida...
+                    </p>
+                </div>
+            </a>
+        </div>
+        <!-- END Story -->
+    </div>
+
+    <!-- Pagination -->
+    <nav aria-label="Page navigation">
+        <ul class="pagination justify-content-center push">
+            <li class="page-item active">
+                <a class="page-link" href="javascript:void(0)">1</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="javascript:void(0)">2</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="javascript:void(0)">3</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="javascript:void(0)">4</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="javascript:void(0)">5</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="javascript:void(0)" aria-label="Next">
+                    <span aria-hidden="true">
+                        <i class="fa fa-angle-right"></i>
+                    </span>
+                    <span class="visually-hidden">Next</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- END Pagination -->
+</div>
---
+++ resources/views/partials/started.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-body-dark">
+    <div class="content content-full">
+        <div class="my-5 text-center">
+            <h3 class="fw-bold mb-2">
+                Do you like our stories?
+            </h3>
+            <h4 class="h5 fw-medium opacity-75">
+                Sign up today and get access to over <strong>10,000</strong> inspiring stories!
+            </h4>
+            <a class="btn btn-primary px-4 py-2" href="javascript:void(0)">Get Started Today</a>
+        </div>
+    </div>
+</div>
