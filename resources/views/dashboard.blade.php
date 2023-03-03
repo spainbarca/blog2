75e7919f65da5cd67fc13c57ede18b60e6084b29
Noah <spain.barcelona.1999@gmail.com>
2023-03-03 02:03:43 -0500

    Dashboard template ok

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,29 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    @section('title','Dashboard')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+    @section('content')
+        <!-- Sidebar -->
+        @include('admin.partials.sidebar')
+        <!-- END Sidebar -->
+
+        <!-- Hero -->
+        @include('admin.partials.hero')
+        <!-- END Hero -->
+
+        <!-- Overview -->
+        @include('admin.partials.overview')
+        <!-- END Overview -->
+
+        <!-- Page Content -->
+        @include('admin.partials.content')
+        <!-- END Page Content -->
+    @endsection
+
+    @section('scripts')
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('js/plugins/chart.js/chart.min.js') }}"></script>
+
+    <!-- Page JS Code -->
+    <script src="{{ asset('js/pages/be_pages_dashboard.min.js') }}"></script>
+    @endsection
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,19 +2,46 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+        <meta name="description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
+
+        <!-- Open Graph Meta -->
+        <meta property="og:title" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework">
+        <meta property="og:site_name" content="OneUI">
+        <meta property="og:description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title',' OneUI - Bootstrap 5 Admin Template &amp; UI Framework')</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Icons -->
+        <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+        <!-- END Icons -->
+
+        <!-- OneUI framework -->
+        <link rel="stylesheet" id="css-main" href="{{ asset('css/oneui.min.css') }}">
+        @yield('css')
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="{{ asset('js/oneui.app.min.js') }}"></script>
+        @yield('scripts')
+
     </head>
-    <body class="font-sans antialiased">
+    <body id="page-container"
+    class="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed main-content-narrow font-sans antialiased"
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
@@ -27,10 +54,20 @@
                 </header>
             @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @include('partials.aside')
+        <!-- END Side Overlay -->
+
+        <!-- Header -->
+        @include('admin.partials.header')
+        <!-- END Header -->
+
+            <div class="content">
+                @yield('content')
+            </div>
+
+            <!-- Footer -->
+            @include('partials.footer')
+            <!-- END Footer -->
         </div>
     </body>
 </html>
