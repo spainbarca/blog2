1de8ce218da597e8603c1687504bf32acf90012e
Noah <spain.barcelona.1999@gmail.com>
2023-03-05 00:31:27 -0500

    Seeders con cursos

--- database/seeders/CursoSeeder.php
+++ database/seeders/CursoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Curso;
 
 class CursoSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class CursoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $curso = new Curso();
+
+        $curso->name = 'Laravel';
+        $curso->description = "El mejor framework de PHP";
+        $curso->category = "Desarrollo web";
+
+        $curso->save();
+
+
+        $curso2 = new Curso();
+
+        $curso2->name = 'Python';
+        $curso2->description = "El mejor lenguaje web";
+        $curso2->category = "Desarrollo web";
+
+        $curso2->save();
+
+
+        $curso3 = new Curso();
+
+        $curso3->name = 'React';
+        $curso3->description = "El mejor library de PHP";
+        $curso3->category = "Desarrollo web";
+
+        $curso3->save();
     }
 }
