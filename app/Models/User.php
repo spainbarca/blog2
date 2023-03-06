5041deef7e76e0e55e55aa650aed3c9a716f7bc5
Noah <spain.barcelona.1999@gmail.com>
2023-03-05 23:10:51 -0500

    Attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +43,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // protected function name(): Attribute
+    // {
+    //     return new Attribute(
+    //         get: fn($value) => ucwords($value);
+    //     );
+
+    //     return Attribute::make(
+    //         get: fn (string $value) => ucfirst($value),
+    //     );
+    // }
+
+    public function getNameAttribute($value){
+        return ucwords($value);
+    }
+    public function setNameAttribute($value){
+        $this->attributes['name'] =strtolower($value);
+    }
 }
